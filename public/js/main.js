cbc6314b5aa4736d3cd467c1573e1ad8829fc2da
maxim-buslaev <maksimys941@gmail.com>
2017-07-18 17:46:00 +0300

    + main

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+var owl = $('.owl-carousel-for-1');
+owl.owlCarousel({
+  margin: 10,
+  nav: true,
+  navText: true,
+  loop: true,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1
+    }
+  }
+})
