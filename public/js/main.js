2899d71cdc6d0d8056242d8df9e8b0def69ae4ca
maxim-buslaev <maksimys941@gmail.com>
2017-07-20 12:09:17 +0300

    Up

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,30 @@
+function filters_button_mobile() {
+  $(".filters .catalog-filters__header .button").click(function () {
+    if (!$(this).hasClass('active')) {
+      $(".filters #filters_list").css("max-height", "none");
+      $(this).addClass('active');
+    } else {
+      $(".filters #filters_list").css("max-height", "0");
+      $(this).removeClass('active');
+    }
+  });
+}
+
+
+$(document).ready(function ($) {
+  filters_button_mobile();
+});
+
+// $("#check_1_1").click(function () {
+//   $("#check_1_1").attr("checked", "checked");
+// });
+
+// $("#uncheck_1_1").click(function () {
+//   $("#check_1_1").removeAttr("checked");
+// });
+
+// document.getElementById('uncheck_1_1').onclick = function () { document.getElementById('check_1_1').checked = "false" }
+
 var owl = $('.owl-carousel-for-1');
 owl.owlCarousel({
   margin: 10,
@@ -11,18 +38,3 @@ owl.owlCarousel({
     }
   }
 })
-
-$(".filters .catalog-filters__header .button").click(function () {
-  $(".filters #filters_list").css("max-height", "none");
-});
-
-$("#check_1_1").click(function () {
-  $("#check_1_1").attr("checked", "checked");
-});
-
-$("#uncheck_1_1").click(function () {
-  $("#check_1_1").removeAttr("checked");
-});
-
-// document.getElementById('uncheck_1_1').onclick = function () { document.getElementById('check_1_1').checked = "false" }
-
