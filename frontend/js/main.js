fd13d48add3f634361db6e64df18c4fd1db491df
maxim-buslaev <maksimys941@gmail.com>
2017-07-21 16:45:33 +0300

    + add others menu, fix structure for header and footer menu, add hovers and etc

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,6 @@
+var currentWidthWindow = document.documentElement.clientWidth;
+const tabletSize = 768, desktopSize = 1024;
+
 function filters_button_mobile() {
   $(".filters .catalog-filters__header .button").click(function () {
     if (!$(this).hasClass('active')) {
@@ -25,17 +28,59 @@ function view_mode_catalog() {
       $(".content.catalog.catalog-page .click-filters .position a.active").removeClass('active');
       $(".content.catalog.catalog-page .items").attr("class", "items " + $(this).attr("class"));
       $(this).addClass('active');
-      // 
-
     }
   });
 }
 
+function sort_mode_catalog() {
+  $(".content.catalog.catalog-page .click-filters .sort a").click(function () {
+    if (!$(this).hasClass('active')) {
+      $(".content.catalog.catalog-page .click-filters .sort a.active").removeClass('active');
+      $(this).addClass('active');
+    }
+  });
+}
+
+function menu_all_products() {
+  $(".header .all-product > a").click(function () {
+    if (!$(".header .all-product").hasClass('active')) {
+      $(".header .all-product .menu").css("max-height", "none");
+      $(".header .all-product").addClass('active');
+    }
+  });
+  $('.header .all-product .menu .title .close').click(function () {
+    if ($(".header .all-product").hasClass('active')) {
+      $(".header .all-product .menu").css("max-height", "0");
+      $(".header .all-product").removeClass('active');
+    }
+  });
+}
+
+function menu_gamburger() {
+  $('.header .top-menu .gamburger').click(function () {
+    if (!$(".header .top-menu").hasClass('active')) {
+      $('.header .top-menu nav').css('display', 'flex');
+      $(".header .top-menu").addClass('active');
+    }
+  });
+  $('.header .top-menu nav .close .button').click(function () {
+    if ($(".header .top-menu").hasClass('active')) {
+      $('.header .top-menu nav').css('display', 'none');
+      $(".header .top-menu").removeClass('active');
+    }
+  });
+}
 
 $(document).ready(function ($) {
   filters_button_mobile();
   preset_button_catalog();
   view_mode_catalog();
+  sort_mode_catalog();
+  menu_all_products();
+
+  if (currentWidthWindow < tabletSize) {
+    menu_gamburger();
+  }
 });
 
 // $("#check_1_1").click(function () {
